2db7d0cdf2a2772f0a09042c77992dd2a7451a49
Ziyovuddin <z.shamsiddini@gmail.com>
2018-11-17 20:52:19 -0500

    working navigation menu with drawers

--- index.js
+++ index.js
@@ -3,5 +3,58 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import { Navigation } from 'react-native-navigation';
+Navigation.registerComponent('example.FirstTabScreen', () => App);
+
+
+
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'example.FirstTabScreen', // this is a registered name for a screen
+      navigatorStyle: {
+        navBarHidden: true,
+      }
+    },
+    {
+      label: 'Two',
+      screen: 'example.FirstTabScreen',
+      navigatorStyle: {
+        navBarHidden: true
+      }
+    }
+  ],
+  tabsStyle: {
+    tabBarHidden: true,
+    tabBarTranslucent: true
+  },
+  drawer: { // optional, add this if you want a side menu drawer in your app
+    left: { // optional, define if you want a drawer from the left
+      screen: 'example.FirstTabScreen', // unique ID registered with Navigation.registerScreen
+      passProps: {}, // simple serializable object that will pass as props to all top screens (optional),
+      fixedWidth: 500, // a fixed width you want your left drawer to have (optional)
+    },
+    right: { // optional, define if you want a drawer from the right
+      screen: 'example.FirstTabScreen', // unique ID registered with Navigation.registerScreen
+      passProps: {}, // simple serializable object that will pass as props to all top screens (optional)
+      fixedWidth: 500, // a fixed width you want your right drawer to have (optional)
+    },
+    style: { // ( iOS only )
+      drawerShadow: true, // optional, add this if you want a side menu drawer shadow
+      contentOverlayColor: 'rgba(0,0,0,0.25)', // optional, add this if you want a overlay color when drawer is open
+      leftDrawerWidth: 50, // optional, add this if you want a define left drawer width (50=percent)
+      rightDrawerWidth: 100, // optional, add this if you want a define right drawer width (50=percent)
+      shouldStretchDrawer: true // optional, iOS only with 'MMDrawer' type, whether or not the panning gesture will “hard-stop” at the maximum width for a given drawer side, default : true
+    },
+    type: 'MMDrawer', // optional, iOS only, types: 'TheSideBar', 'MMDrawer' default: 'MMDrawer'
+    animationType: 'parallax', //optional, iOS only, for MMDrawer: 'door', 'parallax', 'slide', 'slide-and-scale'
+                                        // for TheSideBar: 'airbnb', 'facebook', 'luvocracy','wunder-list'
+    disableOpenGesture: false // optional, can the drawer be opened with a swipe instead of button
+  },
+
+});
+
+
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './components/Welcome';
 
 export default class App extends React.Component{
